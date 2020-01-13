107b87d1d004c8797997ff78dd6608015a8660a5
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-01-13 22:50:04 +1300

    initialized factories section

---
+++ 3. Factories/Factories/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factories
+{
+    // Reason for using Factories Pattern
+    
+    // Object creation logic becomes too convoluted
+    // Constructor is not descriptive
+    // - Name mandated by name of containing type
+    // - Cannot overload with same sets of arguments with different names
+    // - Can turn into 'optional parameter hell'
+    // Object creation (non-piecewise, unlike Builder) can be outsourced to
+    // - A separate function (Factory Method)
+    // - Tha may exist in a separate class (Factory)
+    // - Can create hierarchy of factoreis with Abstract Factory
+    
+    // Factory is
+    // A component responsible solely for the wholesale (not piecewise like builder)
+    // creation of objects
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
