4c6c5dee48a017ed3d23c88936493371dbcf7a03
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-01-23 21:38:03 +1300

    added Singleton unit test to see if only one instance is initiated

--- 5. Singleton/Singleton/Singleton.cs
+++ 5. Singleton/Singleton/Singleton.cs
@@ -22,7 +22,7 @@ namespace Singleton
         // so its not instantiated more than once
         private SingletonDatabase()
         {
-            instanceCount++;
+            instanceCount++; // so we can test how many times it was initiated
             Console.WriteLine("Initializing database");
 
             capitals = File.ReadAllLines(
@@ -36,20 +36,7 @@ namespace Singleton
                 );
         }
 
-        [TestFixture]
-        public class SingletonTests
-        {
-            [Test]
-            public void IsSingletonTest()
-            {
-                var db = SingletonDatabase.Instance;
-                var db2 = SingletonDatabase.Instance;
-                Assert.That(db, Is.SameAs(db2));
-                Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
-            }
-        }
-
-         public int GetPopulation(string name)
+        public int GetPopulation(string name)
         {
             return capitals[name];
         }
@@ -62,5 +49,38 @@ namespace Singleton
              new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
          public static SingletonDatabase Instance => instance.Value;
+    }
+
+    public class SingletonRecordFinder
+    {
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+                result += SingletonDatabase.Instance.GetPopulation(name);
+            return result;
+        }
+    }
+    
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void IsSingletonTest()
+        {
+            var db = SingletonDatabase.Instance;
+            var db2 = SingletonDatabase.Instance;
+            Assert.That(db, Is.SameAs(db2));
+            Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void SingletonTotalPopulationTest()
+        {
+            var rf = new SingletonRecordFinder();
+            var names = new[] {"Seoul", "Mexico City"};
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(17500000 + 17400000));
+        }
     }
 }
