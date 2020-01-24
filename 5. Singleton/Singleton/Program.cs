164268a709521edbee21000d9305298408925bc2
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-01-24 20:56:19 +1300

    finished singleton note

--- 5. Singleton/Singleton/Program.cs
+++ 5. Singleton/Singleton/Program.cs
@@ -9,6 +9,17 @@ namespace Singleton
             var db = SingletonDatabase.Instance;
             var city = "Tokyo";
             Console.WriteLine($"{city} has population of {db.GetPopulation(city)}");
+
+            
+            // Monostate pattern
+            var ceo = new CEO();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+            
+            var ceo2 = new CEO();
+            ceo2.Name = "Kevin Cho";
+            ceo2.Age = 28;
+            Console.WriteLine(ceo);
         }
     }
 }
--- 5. Singleton/Singleton/Singleton.cs
+++ 5. Singleton/Singleton/Singleton.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Autofac;
 using MoreLinq;
 using NUnit.Framework;
 
@@ -51,6 +52,33 @@ namespace Singleton
          public static SingletonDatabase Instance => instance.Value;
     }
 
+    public class OrdinaryDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        // make constructor private
+        // so its not instantiated more than once
+        public OrdinaryDatabase()
+        {
+            Console.WriteLine("Initializing database");
+
+            capitals = File.ReadAllLines(
+                    Path.Combine(
+                        new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "capitals.txt")
+                )
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+
     public class SingletonRecordFinder
     {
         public int GetTotalPopulation(IEnumerable<string> names)
@@ -61,7 +89,38 @@ namespace Singleton
             return result;
         }
     }
-    
+
+    public class ConfigurableRecordFinder
+    {
+        private IDatabase database;
+        
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database ?? throw new ArgumentNullException(nameof(database));
+        }
+        
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+                result += database.GetPopulation(name);
+            return result;
+        }
+    }
+
+    public class DummyDatabase: IDatabase
+    {
+        public int GetPopulation(string name)
+        {
+            return new Dictionary<string, int>
+            {
+                ["alpha"] = 1,
+                ["beta"] = 2,
+                ["gamma"] = 3,
+            }[name];
+        }
+    }
+
     [TestFixture]
     public class SingletonTests
     {
@@ -82,5 +141,28 @@ namespace Singleton
             int tp = rf.GetTotalPopulation(names);
             Assert.That(tp, Is.EqualTo(17500000 + 17400000));
         }
-    }
+
+        [Test]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] {"alpha", "gamma"};
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void DIPopulationTest()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<OrdinaryDatabase>()
+                .As<IDatabase>()
+                .SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+            using (var c = cb.Build())
+            {
+                var rf = c.Resolve<ConfigurableRecordFinder>();
+            }
+        }
+}
 }
