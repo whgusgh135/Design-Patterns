34a1279ad0eb34abd5291c6a5e8d533ffdc10826
Kevin Cho <hcho163@aucklanduni.ac.nz>
2020-01-26 16:47:58 +1300

    added caching

--- 6. Adapter/Adapter/Program.cs
+++ 6. Adapter/Adapter/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq.Expressions;
-using System.Security.AccessControl;
+using System.Linq;
 using MoreLinq;
+using NUnit.Framework;
 using static System.Console;
 
-namespace DotNetDesignPatternDemos.Structural.Adapter.NoCaching
+namespace DotNetDesignPatternDemos.Structural.Adapter.WithCaching
 {
   public class Point
   {
@@ -19,9 +20,30 @@ namespace DotNetDesignPatternDemos.Structural.Adapter.NoCaching
       this.Y = y;
     }
 
+    protected bool Equals(Point other)
+    {
+      return X == other.X && Y == other.Y;
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((Point) obj);
+    }
+
+    public override int GetHashCode()
+    {
+      unchecked
+      {
+        return (X * 397) ^ Y;
+      }
+    }
+
     public override string ToString()
     {
-      return $"{nameof(X)}: {X}, {nameof(Y)}: {Y}";
+      return $"({X}, {Y})";
     }
   }
 
@@ -35,31 +57,59 @@ namespace DotNetDesignPatternDemos.Structural.Adapter.NoCaching
       this.Start = start;
       this.End = end;
     }
+
+    protected bool Equals(Line other)
+    {
+      return Equals(Start, other.Start) && Equals(End, other.End);
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((Line) obj);
+    }
+
+    public override int GetHashCode()
+    {
+      unchecked
+      {
+        return ((Start != null ? Start.GetHashCode() : 0) * 397) ^ (End != null ? End.GetHashCode() : 0);
+      }
+    }
   }
 
-  public class VectorObject : Collection<Line>
+  public abstract class VectorObject : Collection<Line>
   {
-    
   }
 
   public class VectorRectangle : VectorObject
   {
     public VectorRectangle(int x, int y, int width, int height)
     {
-      Add(new Line(new Point(x,y), new Point(x+width, y) ));
-      Add(new Line(new Point(x+width,y), new Point(x+width, y+height) ));
-      Add(new Line(new Point(x,y), new Point(x, y+height) ));
-      Add(new Line(new Point(x,y+height), new Point(x+width, y+height) ));
+      Add(new Line(new Point(x, y), new Point(x + width, y)));
+      Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
+      Add(new Line(new Point(x, y), new Point(x, y + height)));
+      Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
     }
   }
 
-  public class LineToPointAdapter : Collection<Point>
+  public class LineToPointAdapter : IEnumerable<Point>
   {
     private static int count = 0;
+    static Dictionary<int, List<Point>> cache = new Dictionary<int, List<Point>>();
+    private int hash;
 
     public LineToPointAdapter(Line line)
     {
-      WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}] (no caching)");
+      hash = line.GetHashCode();
+      if (cache.ContainsKey(hash)) return; // we already have it
+
+      WriteLine($"{++count}: Generating points for line [{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}] (with caching)");
+      //                                                 ^^^^
+
+      List<Point> points = new List<Point>();
 
       int left = Math.Min(line.Start.X, line.End.X);
       int right = Math.Max(line.Start.X, line.End.X);
@@ -72,17 +122,31 @@ namespace DotNetDesignPatternDemos.Structural.Adapter.NoCaching
       {
         for (int y = top; y <= bottom; ++y)
         {
-          Add(new Point(left, y));
+          points.Add(new Point(left, y));
         }
-      } else if (dy == 0)
+      }
+      else if (dy == 0)
       {
         for (int x = left; x <= right; ++x)
         {
-          Add(new Point(x, top));
+          points.Add(new Point(x, top));
         }
       }
+
+      cache.Add(hash, points);
+    }
+
+    public IEnumerator<Point> GetEnumerator()
+    {
+      return cache[hash].GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
     }
   }
+  
 
   public class Demo
   {
@@ -98,7 +162,7 @@ namespace DotNetDesignPatternDemos.Structural.Adapter.NoCaching
       Write(".");
     }
 
-    static void Main()
+    static void Main(string[] args)
     {
       Draw();
       Draw();
